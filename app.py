f3e3c48808e8a2076a04cc7fde2ebe3ac0b1994f
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2024-06-15 23:52:57 +0700

    add a template of front end

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from src import fuzzy_topsis as ft
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/fuzzy', methods=['GET'])
+def fuzzy():
+    return render_template('fuzzy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
